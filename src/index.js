629a390a6368135a6233be6154210d50dbdaad5a
Gladys Ndichu <itsgladysbrian@gmail.com>
2022-06-14 23:16:57 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let taskList = new taskList();
+  let newTaskForm = document.getElementById("create-task-form");
+  let newTaskDescription = document.getElementById("new-task-description");
+  let taskUl = document.getElementById("tasks");
+  let taskLister = () => (taskUl.innerHTML = taskList.listTasks());
+
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskList.createNewTask(newTaskDescription.value);
+    e.target.reset();
+    taskLister();
+  });
+
+  taskUl.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON" ) {
+      taskList.deleteTask(e.target.dataset.description);
+      taskLister();
+    }
+  });
+  });
